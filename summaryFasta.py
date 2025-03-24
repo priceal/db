e9feec9bfca51033ee4c71a1c9c9999c84ea0fa9
priceal <priceal@emmanuel.edu>
2025-03-24 16:02:51 -0400

    2025 03 24B

--- summaryFasta.py
+++ summaryFasta.py
@@ -21,52 +21,76 @@ list of PDB ids.
 
 """
 
-import requests
-import json
 import pandas as pd
-from itertools import batched
 import os
 from Bio import SeqIO
 
-# define script variables            
-fastaDirectory = '../DATA/db/fasta'            # directory to contain fasta files
-maxNumber = 20      # maximum number to download (limit to first maxNumber ids)
-
-
+###############################################################################
+#################### FUNCTIONS ################################################
 ###############################################################################
 def is_dna( seq ):
+    '''
+    
+
+    Args:
+        seq (TYPE): DESCRIPTION.
+
+    Returns:
+        bool: DESCRIPTION.
+
+    '''
     for c in seq:
         if c not in 'ACGT':
             return False
     return True
 
+def contains_U( seq ):
+    return 'U' in seq
+
+###############################################################################
 def is_protein( seq ):
+    '''
+    
+
+    Args:
+        seq (TYPE): DESCRIPTION.
+
+    Returns:
+        bool: DESCRIPTION.
+
+    '''
     for c in seq:
         if c not in 'ARNDCEQGHILKMFPSTWYVX':
             return False
     return True
+
+###############################################################################
+#################### MAIN #####################################################
+###############################################################################
+      
+fastaDirectory = '../DATA/db/fasta'      # directory containing fasta files
+maxNumber = 20   # maximum number to process (limit to first maxNumber files)
+
 ###############################################################################
 
-# create list of files paths
+# create dataframe containing pdbids, paths and empty columns 
 dirList = os.listdir(fastaDirectory)
 pathList = [ os.path.join(fastaDirectory,f) for f in dirList ]
 pdbids = [ p[:4] for p in dirList ]
 dataDf = pd.DataFrame({'pdbid':pdbids, 
                        'path':pathList,
                        'dna':[[]]*len(pdbids),
+                       'U': [[]]*len(pdbids),
                        'protein':[[]]*len(pdbids),
                        'duplex': [[]]*len(pdbids)}
                       )
-   
-'''
-the following will create the directory for sequence files and download the 
-fasta files. Here, all are downloaded from the PDB.
-'''
 
+    
+# loop through data frame and analyze entries
 for i in dataDf.index:
     record = SeqIO.parse( dataDf.at[i,'path'], 'fasta' )
     print( dataDf.at[i,'path'] )
-    d =[]; p = []; entryDNAs=[]
+    d =[]; u=[]; p = []; entryDNAs=[]
     for entry  in record:
         print('\t',entry.id, is_dna( entry.seq),len(entry.seq),end=' ')
         if is_dna( entry.seq ):
@@ -76,19 +100,21 @@ for i in dataDf.index:
         elif is_protein( entry.seq ):
             p.append(len(entry.seq))
             print('')
+        elif contains_U( entry.seq ):
+            u.append( len(entry.seq))
         else:
             print('unrecognized residue')
-        
+    # add results to dataframe       
     dataDf.at[i,'dna']=d
     dataDf.at[i,'protein']=p
+    dataDf.at[i,'U']=u
+    # check for duplex dna by looking for complementary ssDNAs
     if len( entryDNAs ) == 1:
         duplex= (entryDNAs[0].seq==entryDNAs[0].reverse_complement().seq)
     elif len( entryDNAs ) ==2:
         duplex= (entryDNAs[0].seq==entryDNAs[1].reverse_complement().seq)
     dataDf.at[i,'duplex'] = duplex
     
-            
-    # the fasta file - n.b. need upper case for these files!
-
         
-        
+    
+    
