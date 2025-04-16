9a4e5a8c717fd3a9472f81f55d5a1fc335a414fa
priceal <priceal@emmanuel.edu>
2025-04-16 12:42:33 -0400

    2025 04 16

--- prepFastaCluster.py
+++ prepFastaCluster.py
@@ -9,7 +9,7 @@ tools such as mmseqs
 
 note: protein testing is imperfect, as is based on sequence letters.
 If the chain has at least one letter from the list of symbols that only
-code for amino acids (i.e., at least on that is not ACTGU), then it's
+code for amino acids (i.e., at least on that is not ACTGUN), then it's
 classified protein
 
 better way --- analyze summary info from PDBe to determine exactly which 
@@ -25,11 +25,11 @@ from Bio import SeqIO
 
 # inputs
 fastaDirectory = '../DATA/db/fasta'
-sampleSize = 100
+sampleSize = 0   # non-zero to choose a subsample of files
 
 # outputs
-outputFile = 'temp2.fasta'
-logFile = 'prepFastaCluster_test_20250415.log'
+outputFile = 'dbFasta1000.fasta'
+logFile = 'prepFastaCluster_dbFasta1000_20250416.log'
 
 '''
 ###############################################################################
@@ -38,7 +38,7 @@ logFile = 'prepFastaCluster_test_20250415.log'
 '''
 def is_protein( chain ):
     '''
-    determines if at least 1 residue in chain is canonical AA residue types 
+    determines if at least 1 residue in chain is canonical AA residue type 
     which does not code for a DNA/RNA base. Uses a set 'trick'
 
     Args:
@@ -57,23 +57,29 @@ def is_protein( chain ):
 '''
 
 listDir = os.listdir(fastaDirectory)
-sampleList = random.sample(listDir,sampleSize)
+if sampleSize==0:
+    sampleList = listDir
+else:
+    sampleList = random.sample(listDir,sampleSize)
 
 with open(logFile,'w') as f:
 
     # load in all fasta files in list, and create a single fasta file
     # with all sequences and save.
-    outputList= []
+    outputList= []; count=0
     for file in sampleList:
-        print(len(outputList),end=' ')
+        count+=1; print(count,end=' ')
         read = SeqIO.parse(os.path.join(fastaDirectory,file), 'fasta' )
-        f.write('-----file: '+file+'\n')
+        f.write('\n-----file: '+file+'\n')
         for record in read:
-            f.write(f'{record.id}\nlength = {len(record)}\n')    
+            f.write(f'\n{record.id}\nlength = {len(record)}\n')    
             f.write(repr(record.seq))
             if is_protein( str(record.seq) ):
+                # terminate id at | -- simplifies id in clustering
+                record.id=record.id[:record.id.find('|') ]
                 outputList.append(record)
                 f.write(f'    ....protein! count at {len(outputList)}\n')
+                
 
 # now run mmseqs2
 SeqIO.write(outputList, outputFile, "fasta")
